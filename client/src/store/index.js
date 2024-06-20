541ee860f0a4976e494415de9955285cdf0fdc33
Milan <milantg14@gmail.com>
2024-06-20 20:00:28 +0545

    fisrt commit

---
+++ client/src/store/index.js
@@ -0,0 +1,14 @@
+import { color } from 'framer-motion';
+import {proxy} from 'valtio';
+
+const state = proxy({
+  intro:true,
+  // color:"#607B7D",
+  color:'#7BA05B',
+  isLogoTexture: true,
+  isFullTexture: false,
+  logoDecal: './threejs.png',
+  fullDecal: './threejs.png'
+});
+
+export default state;
